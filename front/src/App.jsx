ad86f22ef49d83de492feb9fc746cd0de9c3dcc3
Swan <swan.marin@epitech.eu>
2024-01-07 17:13:16 +0100

    side bar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Home from './pages/Home';
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
 
 export default function App() {
     
   return (
     <BrowserRouter>
       <Header />
+      <SideBar />
       <Routes>
           <Route path="/" element={<Home />} />
       </Routes>
